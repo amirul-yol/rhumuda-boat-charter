c5197b6df4d1e757bf7f89a6fc2118c6224ccc57
amirul-yol <ahj.professional@gmail.com>
2025-01-27 03:32:35 +0800

    adjust padding for entire system, added layout components

--- rhumudafrontend/src/App.tsx
+++ rhumudafrontend/src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Layout from "./components/Layout/Layout";
 import TestPage from "./pages/TestPage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
-        <Route path="/test" element={<TestPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/test" element={<TestPage />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
--- rhumudafrontend/src/components/Header/Header.tsx
+++ rhumudafrontend/src/components/Header/Header.tsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { AppBar, Toolbar, Box } from "@mui/material";
+import { AppBar, Toolbar, Box, Container } from "@mui/material";
 import logo from "../../assets/images/logo-rhumuda.PNG";
+import { SYSTEM_PADDING } from "../../constants/layout";
 
 const Header: React.FC = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: "white" }}>
-      <Toolbar>
-        <Box
-          component="img"
-          src={logo}
-          alt="Rhumuda Charter Logo"
-          sx={{
-            height: 50,
-            marginRight: 2,
-          }}
-        />
-      </Toolbar>
+      <Container maxWidth={false} disableGutters sx={{ px: SYSTEM_PADDING.x }}>
+        <Toolbar disableGutters>
+          <Box
+            component="img"
+            src={logo}
+            alt="Rhumuda Charter Logo"
+            sx={{
+              height: 50,
+              marginRight: 2,
+            }}
+          />
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
